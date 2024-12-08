f4ed9f6f4dda19be222fb434437214a9b1ab3278
ChiragS007 <chiragsharmat@gmail.com>
2024-12-09 00:00:37 +0530

    first commit hell2heaven

---
+++ src/main/java/com/example/hell2heaven/Hell2heavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.hell2heaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hell2heavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hell2heavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hell2heaven/Hell2heavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hell2heaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hell2heavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
