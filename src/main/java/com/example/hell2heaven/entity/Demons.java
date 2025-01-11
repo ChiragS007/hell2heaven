6f51af194bf6159a483d09235aca59e29cb15890
ChiragS007 <chiragsharmat@gmail.com>
2025-01-11 14:15:45 +0530

    correction

--- src/main/java/com/example/hell2heaven/entity/Demons.java
+++ src/main/java/com/example/hell2heaven/entity/Demons.java
@@ -1,6 +1,14 @@
 package com.example.hell2heaven.entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Demons {
 
-
+    private String Username;
+    private String Password;
 }
--- src/main/java/com/example/hell2heaven/services/DemonSvc.java
+++ src/main/java/com/example/hell2heaven/services/DemonSvc.java
@@ -2,13 +2,8 @@ package com.example.hell2heaven.services;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
+
 public class DemonSvc {
 
-    private String Username;
-    private String Password;
+
 }
